0008d0d644a6cdb4ca4f65981d91ee41537be4b7
scripterhub <119249811+scripterhub@users.noreply.github.com>
2022-12-15 17:24:41 +0100

    Update .lua

--- .lua
+++ .lua
@@ -1,12 +1,12 @@
 
 local RunService = game:GetService("RunService")
-
 RunService.Heartbeat:Connect(function()
     pcall(function()
         for i,v in pairs(game.Players:GetChildren()) do
             if v.Name ~= game.Players.LocalPlayer.Name then
                 local hrp = v.Character.HumanoidRootPart
                 hrp.AssemblyLinearVelocity = Vector3.new(0, 0, 0)   
+                hrp.HipHeight = 1.85
             end
         end
     end)
@@ -18,41 +18,4 @@ local hrp = CPlayer.Character.HumanoidRootPart
                 hrp.Velocity = Vector3.new(0, 0, hrp.Velocity.Y, 0)    
                 hrp.AssemblyLinearVelocity = Vector3.new(0, 0, hrp.Velocity.Y, 0)
 
-getgenv().VelocityChanger = true
-getgenv().Velocity = Vector3.new(1,0,1)
 
-
-local Players     = game:GetService("Players")
-local RunService  = game:GetService("RunService")
-
-local LocalPlayer = Players.LocalPlayer
-local Character   = LocalPlayer.Character
-local RootPart    = Character:FindFirstChild("HumanoidRootPart")
-
-local Heartbeat, RStepped, Stepped = RunService.Heartbeat, RunService.RenderStepped, RunService.Stepped
-
-LocalPlayer.CharacterAdded:Connect(function(NewCharacter)
-   Character = NewCharacter
-end)
-
-local RVelocity, YVelocity = nil, 0.1
-
-while true do
-   if VelocityChanger then
-       --// this a dumb check asnilsadsa
-       if (not RootPart) or (not RootPart.Parent) or (not RootPart.Parent.Parent) then
-           warn("weird ahh died")
-           RootPart = Character:FindFirstChild("HumanoidRootPart")
-       else
-           RVelocity = RootPart.Velocity
-   
-           RootPart.Velocity = type(Velocity) == "vector" and Velocity or Velocity(RVelocity)
-       
-           RStepped:wait()
-       
-           RootPart.Velocity = RVelocity
-       end
-   end
-   
-   Heartbeat:wait()
-end
